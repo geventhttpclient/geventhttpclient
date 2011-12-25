02553e6a9fec595b535b10d4bffc37329f8eb7db
Antonin Amand <antonin.amand@gmail.com>
2011-12-25 21:32:05 +0100

    add benchmarks and examples

---
+++ benchmarks/httpclient.py
@@ -0,0 +1,31 @@
+import time
+import gevent.pool
+from geventhttpclient import HTTPClient, URL
+
+N = 10000
+C = 100
+
+url = URL('http://127.0.0.1/index.html')
+qs = url.query_string
+
+def run(client):
+    response = client.get(qs)
+    response.read()
+    assert response.status_code == 200
+
+
+client = HTTPClient.from_url(url, concurrency=C)
+group = gevent.pool.Pool(size=C)
+
+now = time.time()
+for _ in xrange(N):
+    group.spawn(run, client)
+group.join()
+
+delta = time.time() - now
+req_per_sec = N / delta
+
+print "request count:%d, concurrenry:%d, %f req/s" % (
+    N, C, req_per_sec)
+
+
---
+++ benchmarks/httplib2_simple.py
@@ -0,0 +1,55 @@
+import httplib2
+import time
+import gevent.queue
+import gevent.pool
+from contextlib import contextmanager
+from gevent import monkey
+monkey.patch_all()
+
+
+class ConnectionPool(object):
+
+    def __init__(self, factory, size=5):
+        self.factory = factory
+        self.queue = gevent.queue.Queue(size)
+        for i in xrange(size):
+            self.queue.put(factory())
+
+    @contextmanager
+    def use(self):
+        el = self.queue.get()
+        yield el
+        self.queue.put(el)
+
+
+def httplib2_factory(*args, **kw):
+    def factory():
+        return httplib2.Http(*args, **kw)
+    return factory
+
+
+N = 1000
+C = 10
+
+url = 'http://127.0.0.1/index.html'
+
+def run(pool):
+    with pool.use() as http:
+        http.request(url)
+
+
+http_pool = ConnectionPool(httplib2_factory(), size=C)
+group = gevent.pool.Pool(size=C)
+
+now = time.time()
+for _ in xrange(N):
+    group.spawn(run, http_pool)
+group.join()
+
+delta = time.time() - now
+req_per_sec = N / delta
+
+print "request count:%d, concurrenry:%d, %f req/s" % (
+    N, C, req_per_sec)
+
+
