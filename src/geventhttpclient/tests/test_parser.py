18d8f152475aeaf3ea24211dc7ed78a6cad12f43
Antonin Amand <antonin.amand@gmail.com>
2011-12-30 08:09:48 +0100

    add ref leaks assertions

--- src/geventhttpclient/tests/test_parser.py
+++ src/geventhttpclient/tests/test_parser.py
@@ -3,18 +3,44 @@ from geventhttpclient._parser import HTTPParseError
 from cStringIO import StringIO
 import pytest
 
+from functools import wraps
+import sys
+
 RESPONSE = 'HTTP/1.1 301 Moved Permanently\r\nLocation: http://www.google.fr/\r\nContent-Type: text/html; charset=UTF-8\r\nDate: Thu, 13 Oct 2011 15:03:12 GMT\r\nExpires: Sat, 12 Nov 2011 15:03:12 GMT\r\nCache-Control: public, max-age=2592000\r\nServer: gws\r\nContent-Length: 218\r\nX-XSS-Protection: 1; mode=block\r\n\r\n<HTML><HEAD><meta http-equiv="content-type" content="text/html;charset=utf-8">\n<TITLE>301 Moved</TITLE></HEAD><BODY>\n<H1>301 Moved</H1>\nThe document has moved\n<A HREF="http://www.google.fr/">here</A>.\r\n</BODY></HTML>\r\n'
 
-def test_refcount():
-    import gc
-    gc.set_debug(gc.DEBUG_LEAK)
-    try:
-        parser = HTTPResponse()
-        assert parser.feed(RESPONSE), len(RESPONSE)
-        del parser
-    finally:
-        gc.set_debug(0)
+# borrowed from gevent
+# sys.gettotalrefcount is available only with python built with debug flag on
+gettotalrefcount = getattr(sys, 'gettotalrefcount', None)
 
+
+def wrap_refcount(method):
+    if gettotalrefcount is None:
+        return method
+    @wraps(method)
+    def wrapped(*args, **kwargs):
+        import gc
+        gc.disable()
+        gc.collect()
+        deltas = []
+        d = None
+        try:
+            for _ in xrange(4):
+                d = gettotalrefcount()
+                method(*args, **kwargs)
+                if 'urlparse' in sys.modules:
+                    sys.modules['urlparse'].clear_cache()
+                d = gettotalrefcount() - d
+                deltas.append(d)
+                if deltas[-1] == 0:
+                    break
+            else:
+                raise AssertionError('refcount increased by %r' % (deltas, ))
+        finally:
+            gc.collect()
+            gc.enable()
+    return wrapped
+
+@wrap_refcount
 def test_parse():
     parser = HTTPResponse()
     assert parser.feed(RESPONSE), len(RESPONSE)
@@ -22,6 +48,7 @@ def test_parse():
     assert parser.headers_complete
     assert parser.message_complete
 
+@wrap_refcount
 def test_parse_small_blocks():
     parser = HTTPResponse()
     parser.feed(RESPONSE)
@@ -46,6 +73,7 @@ def test_parse_small_blocks():
         ('x-xss-protection', '1; mode=block'),
     ]
 
+@wrap_refcount
 def test_parse_error():
     response =  HTTPResponse()
     try:
@@ -58,6 +86,7 @@ def test_parse_error():
     else:
         assert False, "should have raised"
 
+@wrap_refcount
 def test_incomplete_response():
     response = HTTPResponse()
     response.feed("""HTTP/1.1 200 Ok\r\nContent-Length:10\r\n\r\n1""")
@@ -66,49 +95,32 @@ def test_incomplete_response():
     assert response.should_keep_alive()
     assert response.should_close()
 
+@wrap_refcount
 def test_response_too_long():
     response = HTTPResponse()
     data = """HTTP/1.1 200 Ok\r\nContent-Length:1\r\n\r\ntoolong"""
     with pytest.raises(HTTPParseError):
         response.feed(data)
 
+@wrap_refcount
+def test_on_body_raises():
+    response = HTTPResponse()
 
-STATUS_CODES = {
-  100 : 'Continue',
-  101 : 'Switching Protocols',
-  200 : 'OK',
-  201 : 'Created',
-  202 : 'Accepted',
-  203 : 'Non-Authoritative Information',
-  204 : 'No Content',
-  205 : 'Reset Content',
-  206 : 'Partial Content',
-  300 : 'Multiple Choices',
-  301 : 'Moved Permanently',
-  302 : 'Moved Temporarily',
-  303 : 'See Other',
-  304 : 'Not Modified',
-  305 : 'Use Proxy',
-  400 : 'Bad Request',
-  401 : 'Unauthorized',
-  402 : 'Payment Required',
-  403 : 'Forbidden',
-  404 : 'Not Found',
-  405 : 'Method Not Allowed',
-  406 : 'Not Acceptable',
-  407 : 'Proxy Authentication Required',
-  408 : 'Request Time-out',
-  409 : 'Conflict',
-  410 : 'Gone',
-  411 : 'Length Required',
-  412 : 'Precondition Failed',
-  413 : 'Request Entity Too Large',
-  414 : 'Request-URI Too Large',
-  415 : 'Unsupported Media Type',
-  500 : 'Internal Server Error',
-  501 : 'Not Implemented',
-  502 : 'Bad Gateway',
-  503 : 'Service Unavailable',
-  504 : 'Gateway Time-out',
-  505 : 'HTTP Version not supported'
-}
+    def on_body(buf):
+        raise RuntimeError('error')
+
+    response._on_body = on_body
+    with pytest.raises(RuntimeError):
+        response.feed(RESPONSE)
+
+@wrap_refcount
+def test_on_message_begin():
+    response = HTTPResponse()
+
+    def on_message_begin():
+        raise RuntimeError('error')
+
+    response._on_message_begin = on_message_begin
+    with pytest.raises(RuntimeError):
+        response.feed(RESPONSE)
+
