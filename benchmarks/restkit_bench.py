cdb0ae014d51acd2c899ca4a3fc83f00cdbf230b
cloudaice <cloudaice@163.com>
2012-07-02 23:21:59 +0800

    fixed restkit_bench.py because the restkit version is too old

--- benchmarks/restkit_bench.py
+++ benchmarks/restkit_bench.py
@@ -6,21 +6,19 @@ if __name__ == "__main__":
     import gevent.pool
     import time
 
-    from restkit import request
-    from restkit.globals import set_manager
-    from restkit.manager.mgevent import GeventManager
+    from restkit import *
+    from socketpool import ConnectionPool
 
     url = 'http://127.0.0.1/index.html'
 
     N = 1000
     C = 10
 
-    manager = GeventManager(timeout=300, max_conn=C, reap_connections=True)
-    set_manager(manager)
+    Pool = ConnectionPool(factory=Connection,backend="gevent",max_size=C,timeout=300)
 
 
     def run():
-        response = request(url)
+        response = request(url,follow_redirect=True,pool=Pool)
         response.body_string()
         assert response.status_int == 200
 
@@ -37,5 +35,4 @@ if __name__ == "__main__":
 
     print "request count:%d, concurrenry:%d, %f req/s" % (
         N, C, req_per_sec)
-
 
