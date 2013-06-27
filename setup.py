a47329ede2d3bdc381e72cccf409c0dcd20db99b
Lucas Clemente Vella <lvella@gmail.com>
2013-06-27 01:26:15 -0300

    Using imp.find_module() to find the module for me.

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ from setuptools.extension import Extension
 from setuptools import find_packages
 
 parser_sources = ['ext/http_parser.c']
-
 if '__pypy__' not in sys.builtin_module_names:
+    # Normal CPython module will be built
     parser_sources.append('ext/_parser.c')
+    extension_name = 'geventhttpclient._parser'
+else:
+    # CFFI helper module will be built (doesn't use CPython API)
+    extension_name = 'geventhttpclient._cffi__parser_helper'
 
-httpparser = Extension('geventhttpclient._parser',
-                    sources = parser_sources,
-                    include_dirs = ['ext'])
+httpparser = Extension(extension_name,
+                       sources = parser_sources,
+                       include_dirs = ['ext'])
 
 setup(name='geventhttpclient',
        version = '1.1',
