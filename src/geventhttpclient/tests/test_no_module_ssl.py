4a55c5b26980214096c7093b95690d60308cb2c0
Michael Löffler <ml@redcowmedia.de>
2015-02-01 18:06:39 +0200

    Tests for no-ssl fixed and rewritten for py.test

--- src/geventhttpclient/tests/test_no_module_ssl.py
+++ src/geventhttpclient/tests/test_no_module_ssl.py
@@ -1,41 +1,37 @@
 import sys
-from unittest import TestCase
+import pytest
 
-
-def delete_item(obj, item):
-    try:
-        del obj[item]
-    except KeyError:
-        pass
-
+import gevent
+import gevent.ssl
 
 class DisableSSL(object):
     def __enter__(self):
+        self._modules = dict()
         # pretend there is no ssl support
-        sys.modules["ssl"] = None
+        self._modules['ssl'] = sys.modules.pop('ssl', None)
+        sys.modules['ssl'] = None
 
         # ensure gevent must be re-imported to fire an ssl ImportError
-        delete_item(sys.modules, "gevent")
-        delete_item(sys.modules, "gevent.ssl")
-        delete_item(sys.modules, "geventhttpclient")
-        delete_item(sys.modules, "geventhttpclient.response")
-        delete_item(sys.modules, "geventhttpclient.httplib")
+        for module_name in [k for k in sys.modules.keys() if k.startswith('gevent')]:
+            self._modules[module_name] = sys.modules.pop(module_name)
 
     def __exit__(self, *args, **kwargs):
-        if 'ssl' in sys.modules:
-            del sys.modules["ssl"]
+        # Restore all previously disabled modules
+        sys.modules.update(self._modules) 
 
 
-class TestNoModuleSSL(TestCase):
-    def run(self, *args, **kwargs):
-        with DisableSSL():
-            super(TestNoModuleSSL, self).run(*args, **kwargs)
-
-    def test_import_with_nossl(self):
+def test_import_with_nossl():
+    with DisableSSL():
         from geventhttpclient import httplib
         from geventhttpclient import HTTPClient
 
-    def test_httpclient_raises_with_no_ssl(self):
+def test_httpclient_raises_with_no_ssl():
+    with DisableSSL():
         from geventhttpclient import HTTPClient
-        with self.assertRaises(Exception):
+        with pytest.raises(Exception):
             HTTPClient.from_url("https://httpbin.org/")
+            
+
+if __name__ == '__main__':
+    test_import_with_nossl()
+    test_httpclient_raises_with_no_ssl()
