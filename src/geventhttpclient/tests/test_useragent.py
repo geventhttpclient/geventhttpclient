f3766aeac1db18de2c8d51e9b49e241fb33b31d8
Roman Mohr <rmohr@redhat.com>
2016-07-25 13:29:21 +0200

    Add useragent test for file posts

--- src/geventhttpclient/tests/test_useragent.py
+++ src/geventhttpclient/tests/test_useragent.py
@@ -1,5 +1,9 @@
-from contextlib import contextmanager
 import gevent.pywsgi
+import os
+import six
+import tempfile
+
+from contextlib import contextmanager
 from geventhttpclient.useragent import UserAgent
 
 
@@ -13,9 +17,10 @@ def wsgiserver(handler):
         server.stop()
 
 
-def check_upload(body, body_length):
+def check_upload(body, headers=None):
     def wsgi_handler(env, start_response):
-        assert int(env.get('CONTENT_LENGTH')) == body_length
+        if headers:
+            assert six.viewitems(env) >= six.viewitems(headers)
         assert body == env['wsgi.input'].read()
         start_response('200 OK', [])
         return []
@@ -34,14 +39,31 @@ def check_redirect():
     return wsgi_handler
 
 
+def test_file_post():
+    body = tempfile.NamedTemporaryFile("a+b", delete=False)
+    name = body.name
+    try:
+        body.write(b"123456789")
+        body.close()
+        headers = {'CONTENT_LENGTH': '9', 'CONTENT_TYPE': 'application/octet-stream'}
+        with wsgiserver(check_upload(b"123456789", headers)):
+            useragent = UserAgent()
+            with open(name, 'rb') as body:
+                useragent.urlopen('http://127.0.0.1:54323/', method='POST', payload=body)
+    finally:
+        os.remove(name)
+
+
 def test_string_post():
-    with wsgiserver(check_upload(b"12345", 5)):
+    headers = {'CONTENT_LENGTH': '5', 'CONTENT_TYPE': 'application/octet-stream'}
+    with wsgiserver(check_upload(b"12345", headers)):
         useragent = UserAgent()
         useragent.urlopen('http://127.0.0.1:54323/', method='POST', payload="12345")
 
 
 def test_bytes_post():
-    with wsgiserver(check_upload(b"12345", 5)):
+    headers = {'CONTENT_LENGTH': '5', 'CONTENT_TYPE': 'application/octet-stream'}
+    with wsgiserver(check_upload(b"12345", headers)):
         useragent = UserAgent()
         useragent.urlopen('http://127.0.0.1:54323/', method='POST', payload=b"12345")
 
