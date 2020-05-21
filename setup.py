ece4011dba3e223249be810aa9c119d954923cfe
Antonin Amand <antonin.amand@gmail.com>
2020-05-21 12:31:19 +0200

    bump version 1.3.2

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if sys.hexversion < 0x02070900:
     ]
 
 setup(name='geventhttpclient',
-      version = '1.3.1dev',
+      version = '1.3.2',
       description = 'http client library for gevent',
       long_description = DESC,
       url="http://github.com/gwik/geventhttpclient",
--- src/geventhttpclient/__init__.py
+++ src/geventhttpclient/__init__.py
@@ -1,6 +1,6 @@
 # package
 
-__version__ = "1.3.1dev"
+__version__ = "1.3.2"
 
 from geventhttpclient.client import HTTPClient
 from geventhttpclient.url import URL
