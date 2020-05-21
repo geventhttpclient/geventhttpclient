0de61adbf514850e4b0f106abcef3758f049fae1
Antonin Amand <antonin.amand@gmail.com>
2020-05-21 13:43:58 +0200

    back to dev

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if sys.hexversion < 0x02070900:
     ]
 
 setup(name='geventhttpclient',
-      version = '1.4.0',
+      version = '1.4.0dev',
       description = 'http client library for gevent',
       long_description = DESC,
       url="http://github.com/gwik/geventhttpclient",
