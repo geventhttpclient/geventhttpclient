ce1da405d79c0ebc31aadeae0d974f522c9bf018
Lars Holmberg <lars.holmberg@svenskaspel.se>
2021-08-21 20:19:10 +0200

    Bump version to 1.5.1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ if sys.hexversion < 0x02070900:
     ]
 
 setup(name='geventhttpclient',
-      version = '1.5.0',
+      version = '1.5.1',
       description = 'http client library for gevent',
       long_description = DESC,
       url="http://github.com/gwik/geventhttpclient",
