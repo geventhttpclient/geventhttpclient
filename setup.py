d79022bb27bb4362d97b5b3a3b4e719369d82d75
Lars Holmberg <lars.holmberg@svenskaspel.se>
2022-08-31 09:56:46 +0200

    Bump version to 2.0.2

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if sys.hexversion < 0x02070900:
     ]
 
 setup(name='geventhttpclient',
-      version = '2.0.1',
+      version = '2.0.2',
       description = 'http client library for gevent',
       long_description = DESC,
       url="http://github.com/gwik/geventhttpclient",
--- src/geventhttpclient/__init__.py
+++ src/geventhttpclient/__init__.py
@@ -1,6 +1,6 @@
 # package
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 from geventhttpclient.client import HTTPClient
 from geventhttpclient.url import URL
