f66d83b2fa714141c0a89471f9db2c8d3ed07064
Michael Löffler <michael@loeffler.io>
2024-04-08 11:59:51 +0300

    Fix SSL deprecation warnings

--- src/geventhttpclient/tests/test_ssl.py
+++ src/geventhttpclient/tests/test_ssl.py
@@ -16,6 +16,7 @@ import gevent.server
 import gevent.socket
 import gevent.ssl
 import os
+import ssl
 
 from gevent import joinall
 from gevent.socket import error as socket_error
@@ -45,7 +46,8 @@ def server(handler, backlog=1):
         backlog=backlog,
         handle=handler,
         keyfile=KEY,
-        certfile=CERT)
+        certfile=CERT,
+        ssl_version=ssl.PROTOCOL_TLS_SERVER)
     server.start()
     try:
         yield (server.server_host, server.server_port)
@@ -56,7 +58,7 @@ def server(handler, backlog=1):
 def timeout_connect_server():
     sock = gevent.socket.socket(gevent.socket.AF_INET,
         gevent.socket.SOCK_STREAM, 0)
-    sock = gevent.ssl.wrap_socket(sock, keyfile=KEY, certfile=CERT)
+    sock = gevent.ssl.wrap_socket(sock, keyfile=KEY, certfile=CERT, ssl_version=ssl.PROTOCOL_TLS_SERVER)
     sock.setsockopt(gevent.socket.SOL_SOCKET, gevent.socket.SO_REUSEADDR, 1)
     sock.bind(("localhost", 0))
     sock.listen(1)
