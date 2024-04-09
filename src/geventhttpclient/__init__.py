571f71da33852ced8a3bd62560dbbfb08ee066f1
Lars Holmberg <Lars.Holmberg@svenskaspel.se>
2024-04-09 22:22:42 +0200

    update versioning comments

--- src/geventhttpclient/__init__.py
+++ src/geventhttpclient/__init__.py
@@ -1,6 +1,6 @@
 # package
 
-__version__ = "2.1.0"  # dont forget to update version in setup.py as well
+__version__ = "2.1.0"  # dont forget to update version in pyproject.toml as well
 
 from geventhttpclient.client import HTTPClient
 from geventhttpclient.url import URL
