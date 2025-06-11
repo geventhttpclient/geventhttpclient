406d597a10a5be1e2815507f84243c56420ef879
Michael Löffler <michael@loeffler.io>
2025-06-11 15:34:07 +0300

    Version bump

--- src/geventhttpclient/__init__.py
+++ src/geventhttpclient/__init__.py
@@ -1,6 +1,6 @@
 # package
 
-__version__ = "2.3.3"  # don't forget to update version in pyproject.toml as well
+__version__ = "2.3.4"  # don't forget to update version in pyproject.toml as well
 
 from geventhttpclient.api import delete, get, head, options, patch, post, put, request
 from geventhttpclient.client import HTTPClient
